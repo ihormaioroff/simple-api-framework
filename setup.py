b1cb190a096dc43eff20dbee63ed5d104e0e94f1
Ihor Maiorov <ihor.maioroff@gmail.com>
2022-08-02 22:05:34 +0300

    Version change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 DESCRIPTION = 'Simple async API framework'
 LONG_DESCRIPTION = """
 This package contains simple async API framework, based on Tornado.
